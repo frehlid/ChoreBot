c7863b6c72fb13cca074790f83aec2c61390b2a8
frehlid <frehlid@student.ubc.ca>
2024-03-12 18:37:30 -0700

    allow for users to be in multiple groups:

--- backend.py
+++ backend.py
@@ -12,8 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Chore(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    group = db.Column(db.String(128), nullable=False)
+    group = db.Column(db.JSON)
     name = db.Column(db.String(128), nullable=False) 
     assigned = db.Column(db.String(128), nullable=True)
     completed = db.Column(db.Boolean, default=False)
@@ -22,6 +21,8 @@ class Group(Enum):
     BASEMENT = "Basement"
     MAIN_FLOOR = "Main Floor"
     UPSTAIRS = "Upstairs"
+    MAIN_AND_UPSTAIRS = "Main and Upstairs"
+    ALL = "All"
 
     @staticmethod 
     def list():
@@ -29,9 +30,8 @@ class Group(Enum):
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
-    group = db.Column(db.String(50), nullable=False)
+    group = db.Column(db.JSON)
     preferences = db.Column(db.JSON)
 
 
@@ -43,12 +43,23 @@ def get_chores():
     chores = [{"group":chore.group, "name":chore.name, "completed":chore.completed} for chore in chores_list]
     return jsonify({'chores':chores})
 
-@app.route('/choresByGroup', methods=["GET"])
-def get_chores_by_group():
+@app.route('/choresByUserGroup', methods=["GET"])
+def get_chores_by_user_group():
     user_name = request.args.get('name')
-    user_group = User.query.filter_by(name=user_name).group
-    chores_list = Chore.query.filter_by(group=user_group)
-    chores = [{"group":chore.group, "name":chore.name, "completed":chore.completed} for chore in chores_list]
+    user_group = User.query.filter_by(name=user_name).first().group
+    return get_chores_by_group(user_group)
+
+@app.route('/choresByGroup', methods=["GET"])
+def chores_by_group():
+    group = request.args.get('group')
+    return get_chores_by_group(group)
+
+def get_chores_by_group(groups):
+    all_chores = Chore.query.all()
+    matching_chores = [chore for chore in all_chores if chore.group in groups]
+
+    chores = [{"group":chore.group, "name":chore.name, "completed":chore.completed} for chore in matching_chores]
+
     return jsonify({'chores':chores})
 
 
@@ -80,10 +91,13 @@ def update_chore_status():
 @app.route('/addUser', methods=["POST"])
 def create_user():
     data = request.json
-    if data['group'] not in Group.list():
+
+    group = data['group']
+
+    if not all(g in Group.list() for g in group):
         return jsonify({'error': 'Invalid group specified'}), 400
 
-    new_user = User(name=data['name'], group=data['group'])
+    new_user = User(name=data['name'], group=group)
         db.session.add(new_user)
         db.session.commit()
         return jsonify({'user': {'id': new_user.id, 'name': new_user.name, 'group': new_user.group}}), 201
@@ -106,6 +120,42 @@ def update_preferences():
     user.preferences = preferences
     db.session.commit()
     return jsonify({'success': True, 'preferences': user.preferences}), 200
+
+def populate_db():
+    if User.query.first() is not None or Chores.query.first() is not None:
+        print("The database already contains data")
+        return
+
+    users = [
+        {"name": "Dieter", "group":[Group.UPSTAIRS.value, Group.ALL.value, Group.MAIN_AND_UPSTAIRS.value]},
+        {"name": "Graydon", "group":[Group.UPSTAIRS.value, Group.ALL.value, Group.MAIN_AND_UPSTAIRS.value]},
+        {"name": "Levi", "group": [Group.MAIN_FLOOR.value, Group.ALL.value, Group.MAIN_AND_UPSTAIRS.value]},
+        {"name": "Astrid", "group":[Group.MAIN_FLOOR.value, Group.ALL.value, Group.MAIN_AND_UPSTAIRS.value]},
+        {"name": "Isaac", "group":[Group.BASEMENT.value, Group.ALL.value]},
+        {"name": "Paul", "group":[Group.BASEMENT.value, Group.ALL.value]},
+        {"name": "Katie", "group":[Group.BASEMENT.value, Group.ALL.value]},
+    ]
+
+    chores = [
+            {"name":"Living Room Floors", "group":Group.ALL.value},
+            {"name":"Kitchen Floors", "group":Group.MAIN_AND_UPSTAIRS.value},
+    ]
+    
+    for user in users:
+        new_user = User(name=user['name'], group=user['group'])
+        db.session.add(new_user)
+
+    for chore in chores:
+        new_chore = Chore(name=chore['name'], group=chore['group'])
+        db.session.add(new_chore)
+
+    db.session.commit()
+    print("Database populated with inital data")
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
